a44bee81f251f379c4b8d2f485eafe75b2aea8d5
cloudshapes <roland.dunn@gmail.com>
2025-06-18 10:09:57 +0100

    further iterations, simpler code

--- acid-house-starfield/main5.js
+++ acid-house-starfield/main5.js
@@ -11,6 +11,7 @@ let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
 const materials = [];
+const particles = [];
 const N_VERTICES = 100;
 
 init();
@@ -20,7 +21,6 @@ function init() {
 	scene = new THREE.Scene();
 	scene.fog = new THREE.FogExp2(0x0000, 0.00025);
 
-	const geometry = new THREE.BufferGeometry();
 
 	const textureLoader = new THREE.TextureLoader();
 	const assignSRGB = (texture) => {
@@ -33,72 +33,75 @@ function init() {
 	const sprite4 = textureLoader.load('ah-4.png', assignSRGB);
 	const sprite5 = textureLoader.load('ah-5.png', assignSRGB);
 
-
-	// Generate n vertices.
-	const vertices = [];
-	for ( let i = 0; i < N_VERTICES; i ++ ) {
-
-		const x = Math.random() * 2000 - 1000;
-		const y = Math.random() * 2000 - 1000;
-		const z = Math.random() * 2000 - 1000;
-
-		vertices.push( x, y, z );
-
-	}
-
-
-	/* 
-	const vertices = [
-	  -500, -500, 0,
-	   500, -500, 0,
-	   500,  500, 0,
-	  -500,  500, 0,
-	     0,    0, 0
-	];
-	*/
-
-	geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
+	// Color, sprite, size:	
 	/*
 	parameters = [
-		[[1.0, 0.2, 0.5], sprite2, 100]
-	];
-	*/
-
-	// Color, sprite, size:
-	
-	parameters = [
-		[[ 1.0, 0.2, 0.5 ], sprite2, 60 ],
+		[[ 1.0, 0.2, 0.5 ], sprite2, 200 ],
 		[[ 0.95, 0.1, 0.5 ], sprite3, 60 ],
 		[[ 0.90, 0.05, 0.5 ], sprite1, 30 ],
 		[[ 0.85, 0, 0.5 ], sprite5, 28 ],
 		[[ 0.80, 0, 0.5 ], sprite4, 25 ]
 	];
+	*/
 
+	// Could we change this so that it is an array of dictionaries
+	// and one of those values in the dictionary is speed?
+	// and another is speed?
+	// 
+
+	// Color, sprite, size:	
+	parameters = [
+		[[ 1.0, 0.2, 0.5 ], sprite2, 200 ],
+		[[ 0.95, 0.1, 0.5 ], sprite3, 60 ]
+	];
+
+	// Use different geometry per sprite:
 	for (let i = 0; i < parameters.length; i++) {
-		const color = parameters[i][0];
-		const sprite = parameters[i][1];
-		const size = parameters[i][2];
+	  const vertices = [];
+	  for (let j = 0; j < N_VERTICES; j++) {
+	    const x = Math.random() * 2000 - 1000;
+	    const y = Math.random() * 2000 - 1000;
+	    const z = Math.random() * 2000 - 1000;
+	    vertices.push(x, y, z);
+	  }
+	  const geometry = new THREE.BufferGeometry();
+	  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-		materials[i] = new THREE.PointsMaterial({
-			size: size,
-			map: sprite,
-			blending: THREE.AdditiveBlending,
-			depthTest: false,
-			transparent: true
-		});
-		materials[i].color.setHSL(color[0], color[1], color[2], THREE.SRGBColorSpace);
+	  const color = parameters[i][0];
+	  const sprite = parameters[i][1];
+	  const size = parameters[i][2];
 
-		const particles = new THREE.Points(geometry, materials[i]);
-		scene.add(particles);
+	  const material = new THREE.PointsMaterial({
+	    size: size,
+	    map: sprite,
+	    blending: THREE.AdditiveBlending,
+	    depthTest: false,
+	    transparent: true
+	  });
+	  material.color.setHSL(color[0], color[1], color[2], THREE.SRGBColorSpace);
+	  materials.push(material);
+
+	  const points = new THREE.Points(geometry, material);
+	  scene.add(points);
+	  particles.push(points);
 	}
 
 
-	// Add box helper:
-	const geometryBox = new THREE.Box3().setFromBufferAttribute(
-	  geometry.getAttribute('position')
-	);
-	const boxHelper = new THREE.Box3Helper(geometryBox, 0xff00ff);
+	// Add overall box helper:
+	const overallBox = new THREE.Box3();
+
+	for (let i = 0; i < scene.children.length; i++) {
+		const object = scene.children[i];
+		if (object instanceof THREE.Points) {
+			const geometry = object.geometry;
+			const objectBox = new THREE.Box3().setFromBufferAttribute(
+				geometry.getAttribute('position')
+			);
+			overallBox.union(objectBox); // expand the overall box to include this one
+		}
+	}
+
+	const boxHelper = new THREE.Box3Helper(overallBox, 0xff00ff);
 	scene.add(boxHelper);
 
 
@@ -113,8 +116,8 @@ function init() {
 
 	const size = new THREE.Vector3();
 	const center = new THREE.Vector3();
-	geometryBox.getSize(size);
-	geometryBox.getCenter(center);
+	overallBox.getSize(size);
+	overallBox.getCenter(center);
 	const maxDim = Math.max(size.x, size.y, size.z);
 	const fov = camera.fov * (Math.PI / 180);
 	let cameraZ = maxDim / (2 * Math.tan(fov / 2));
@@ -122,6 +125,7 @@ function init() {
 	camera.position.set(center.x, center.y, cameraZ);
 	camera.lookAt(center);
 	camera.updateProjectionMatrix();
+
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -163,22 +167,49 @@ function render() {
 
 	const time = Date.now() * 0.00005;
 
+	for (let i = 0; i < particles.length; i++) {
+		const p = particles[i];
+		p.position.z += i / 2;
+		if (p.position.z > 1000) p.position.z -= 2000;
+	}
 
+	/*
 	for ( let i = 0; i < scene.children.length; i ++ ) {
 		const object = scene.children[ i ];
 		if ( object instanceof THREE.Points ) {
+
 			// Rotates the entire THREE.Points object around its X axis (left-to-right line in 3D space).
 			// This is defining the rotation around the X axis which in turn adjusts the Y and Z values
-			object.rotation.x = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+			// object.rotation.x = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+
+			object.position.z +=  i/2;
+      		if(object.position.z>1000) object.position.z-=2000; 
+
 		}
 	}
+	*/
+
+	/*
+    // loop through each star
+    for(var i=0; i<stars.length; i++) {
+      
+      var star = stars[i]; 
+        
+      // and move it forward dependent on the mouseY position. 
+      star.position.z +=  i/10;
+        
+      // if the particle is too close move it to the back
+      if(star.position.z>1000) star.position.z-=2000; 
+      
+    }
+	*/
 
 	for ( let i = 0; i < materials.length; i ++ ) {
 		const color = parameters[ i ][ 0 ];
 		const h = ( 360 * ( color[ 0 ] + time ) % 360 ) / 360;
 		materials[ i ].color.setHSL( h, color[ 1 ], color[ 2 ], THREE.SRGBColorSpace );
 	}
-
+	 
 	renderer.render( scene, camera );
 
 }
--- acid-house-starfield/main6.js
+++ acid-house-starfield/main6.js
@@ -10,7 +10,7 @@ let controls;
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
-const materials = [];
+let sprite_objects = [];
 const N_VERTICES = 100;
 
 init();
@@ -20,13 +20,12 @@ function init() {
 	scene = new THREE.Scene();
 	scene.fog = new THREE.FogExp2(0x0000, 0.00025);
 
-	const geometry = new THREE.BufferGeometry();
-
 	const textureLoader = new THREE.TextureLoader();
 	const assignSRGB = (texture) => {
 		texture.colorSpace = THREE.SRGBColorSpace;
 	};
 
+	
 	const sprite1 = textureLoader.load('ah-1.png', assignSRGB);
 	const sprite2 = textureLoader.load('ah-2.png', assignSRGB);
 	const sprite3 = textureLoader.load('ah-3.png', assignSRGB);
@@ -34,71 +33,73 @@ function init() {
 	const sprite5 = textureLoader.load('ah-5.png', assignSRGB);
 
 
-	// Generate n vertices.
-	const vertices = [];
-	for ( let i = 0; i < N_VERTICES; i ++ ) {
-
-		const x = Math.random() * 2000 - 1000;
-		const y = Math.random() * 2000 - 1000;
-		const z = Math.random() * 2000 - 1000;
-
-		vertices.push( x, y, z );
-
-	}
-
-
-	/* 
-	const vertices = [
-	  -500, -500, 0,
-	   500, -500, 0,
-	   500,  500, 0,
-	  -500,  500, 0,
-	     0,    0, 0
-	];
-	*/
-
-	geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
-	/*
-	parameters = [
-		[[1.0, 0.2, 0.5], sprite2, 100]
-	];
-	*/
-
-	// Color, sprite, size:
-	
-	parameters = [
-		[[ 1.0, 0.2, 0.5 ], sprite2, 60 ],
-		[[ 0.95, 0.1, 0.5 ], sprite3, 60 ],
-		[[ 0.90, 0.05, 0.5 ], sprite1, 30 ],
+	// Color, sprite, size, particles/points, material, z-speed:	
+	sprite_objects = [
+		[[ 1.0, 0.2, 0.5 ], sprite2, 200, null, null, null ],
+		[[ 0.95, 0.1, 0.5 ], sprite3, 60, null, null, null ],
+		[[ 0.90, 0.05, 0.5 ], sprite1, 200, null, null, null ],
 		[[ 0.85, 0, 0.5 ], sprite5, 28 ],
 		[[ 0.80, 0, 0.5 ], sprite4, 25 ]
 	];
 
-	for (let i = 0; i < parameters.length; i++) {
-		const color = parameters[i][0];
-		const sprite = parameters[i][1];
-		const size = parameters[i][2];
 
-		materials[i] = new THREE.PointsMaterial({
-			size: size,
-			map: sprite,
-			blending: THREE.AdditiveBlending,
-			depthTest: false,
-			transparent: true
-		});
-		materials[i].color.setHSL(color[0], color[1], color[2], THREE.SRGBColorSpace);
+	// Use different geometry per sprite:
+	for (let i = 0; i < sprite_objects.length; i++) {
 
-		const particles = new THREE.Points(geometry, materials[i]);
-		scene.add(particles);
+		// Generate vertices:
+	  const vertices = [];
+	  for (let j = 0; j < N_VERTICES; j++) {
+	    const x = Math.random() * 2000 - 1000;
+	    const y = Math.random() * 2000 - 1000;
+	    const z = Math.random() * 2000 - 1000;
+	    vertices.push(x, y, z);
+	  }
+
+	  // Generate geometry:
+	  const geometry = new THREE.BufferGeometry();
+	  geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+
+	  const color = sprite_objects[i][0];
+	  const sprite = sprite_objects[i][1];
+	  const size = sprite_objects[i][2];
+
+	  // Generate materials
+	  const material = new THREE.PointsMaterial({
+	    size: size,
+	    map: sprite,
+	    blending: THREE.AdditiveBlending,
+	    depthTest: false,
+	    transparent: true
+	  });
+	  material.color.setHSL(color[0], color[1], color[2], THREE.SRGBColorSpace);
+		sprite_objects[i][4] = material;
+
+		// Generate points/particles
+	  let points = new THREE.Points(geometry, material);
+	  scene.add(points);
+		sprite_objects[i][3] = points;
+
+		// Assign random speed:
+		sprite_objects[i][5] = random_z_speed();
+
 	}
 
 
-	// Add box helper:
-	const geometryBox = new THREE.Box3().setFromBufferAttribute(
-	  geometry.getAttribute('position')
-	);
-	const boxHelper = new THREE.Box3Helper(geometryBox, 0xff00ff);
+	// Add overall box helper:
+	const overallBox = new THREE.Box3();
+
+	for (let i = 0; i < scene.children.length; i++) {
+		const object = scene.children[i];
+		if (object instanceof THREE.Points) {
+			const geometry = object.geometry;
+			const objectBox = new THREE.Box3().setFromBufferAttribute(
+				geometry.getAttribute('position')
+			);
+			overallBox.union(objectBox); // expand the overall box to include this one
+		}
+	}
+
+	const boxHelper = new THREE.Box3Helper(overallBox, 0xff00ff);
 	scene.add(boxHelper);
 
 
@@ -113,8 +114,8 @@ function init() {
 
 	const size = new THREE.Vector3();
 	const center = new THREE.Vector3();
-	geometryBox.getSize(size);
-	geometryBox.getCenter(center);
+	overallBox.getSize(size);
+	overallBox.getCenter(center);
 	const maxDim = Math.max(size.x, size.y, size.z);
 	const fov = camera.fov * (Math.PI / 180);
 	let cameraZ = maxDim / (2 * Math.tan(fov / 2));
@@ -122,6 +123,7 @@ function init() {
 	camera.position.set(center.x, center.y, cameraZ);
 	camera.lookAt(center);
 	camera.updateProjectionMatrix();
+
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
@@ -160,25 +162,34 @@ function animate() {
 
 
 function render() {
-
 	const time = Date.now() * 0.00005;
 
+	for (let i = 0; i < sprite_objects.length; i++) {
+		let sprite_object = sprite_objects[i];
 
-	for ( let i = 0; i < scene.children.length; i ++ ) {
-		const object = scene.children[ i ];
-		if ( object instanceof THREE.Points ) {
-			// Rotates the entire THREE.Points object around its X axis (left-to-right line in 3D space).
-			// This is defining the rotation around the X axis which in turn adjusts the Y and Z values
-			object.rotation.x = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
-		}
-	}
 
-	for ( let i = 0; i < materials.length; i ++ ) {
-		const color = parameters[ i ][ 0 ];
+		// Adjust sprite positions:
+		const p = sprite_object[3];
+		let speed = sprite_object[5];
+
+		p.position.z += speed;
+		if (p.position.z > 1000) p.position.z -= 2000;
+
+		// Adjust materials:
+		const color = sprite_object[ 0 ];
 		const h = ( 360 * ( color[ 0 ] + time ) % 360 ) / 360;
-		materials[ i ].color.setHSL( h, color[ 1 ], color[ 2 ], THREE.SRGBColorSpace );
-	}
+		let material = sprite_object[ 4 ];
+		material.color.setHSL( h, color[ 1 ], color[ 2 ], THREE.SRGBColorSpace );
+
+	};
 
 	renderer.render( scene, camera );
-
 }
+
+
+function random_z_speed()	{
+	return 1 + Math.random() * 10.0;
+}
+
+
+
